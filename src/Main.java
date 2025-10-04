65518226466720d9db985af58c8d68f7089baf8e
Pinar Bildirici <pinarbildirici@MacBook-Pro-som-tillhor-Pinar.local>
2025-10-04 23:30:19 +0200

    created Board and Player class to folder

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-
-
-
-
-
-        }
+   public class TicTacToe {
+    private Board board;
+    private Player playerX;
+    private Player player0;
+    private Scanner scanner;
+    private Player currentPlayer;
 
 }
